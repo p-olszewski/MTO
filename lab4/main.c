4c2047876cf445bb2a73378b01bdd0cb849a705e
p-olszewski <przemo1297@gmail.com>
2022-11-06 10:45:35 +0100

    LAB4 Change %s to %d

--- lab4/main.c
+++ lab4/main.c
@@ -5,7 +5,7 @@ int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
 		if((format_string[i] == '#') && (format_string[i+1] == 'g')){
 			i++;
-			printf("%s",param);
+			printf("%d",param);
 		}else
 			putchar(format_string[i]);
 	}
