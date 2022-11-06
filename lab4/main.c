0c86ed0803f76688ac6cff171fd3bc5c69af1e51
p-olszewski <przemo1297@gmail.com>
2022-11-06 10:56:06 +0100

    LAB4 Add condition to check param is number

--- lab4/main.c
+++ lab4/main.c
@@ -5,7 +5,9 @@ int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
 		if((format_string[i] == '#') && (format_string[i+1] == 'g')){
 			i++;
-			printf("%d",param);
+			if (isdigit(atoi(param))) {
+				printf("%d",param);
+			}
 		}else
 			putchar(format_string[i]);
 	}
