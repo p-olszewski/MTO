b1b625a3f1ee64339994aa4aa48f09352447e95d
p-olszewski <przemo1297@gmail.com>
2022-11-20 11:20:46 +0100

    LAB5 Add loop to change numbers

--- lab5/main.c
+++ lab5/main.c
@@ -35,12 +35,26 @@ int my_printf(char *format_string, char *param){
 			
 		} else if((format_string[i] == '#') && (format_string[i+1] == 'X') && (format_string[i+2] == 'g')) {
 			int paramIsNumber = 1;
-            for (int j = 0; j < strlen(param); j++) {
+            for (int j = i + 2; j < strlen(param); j++) {
 				if (!isdigit(param[j])) {
 					paramIsNumber = 0;
 					break;
 				}
             }
+
+			if (paramIsNumber) {
+				for (int k = i + 2; k < strlen(param); k++) {
+					if (param[k] == '0') {
+						param[k] = '9';
+					} else {
+						param[k] = atoi(param[k]) - 1;
+					}
+            	}
+				printf("%s", param);
+			} else {
+				puts("");
+				return 0;
+			}
 		}
 		else
 			putchar(format_string[i]);
