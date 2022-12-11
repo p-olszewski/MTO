f89c92249550e59af13142958e40cf5a7e04d08d
p-olszewski <przemo1297@gmail.com>
2022-12-11 10:49:34 +0100

    LAB6 Add number length counter

--- lab6/main.c
+++ lab6/main.c
@@ -3,8 +3,18 @@
 
 int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
-		if((format_string[i] == '#') && (format_string[i+1] == '.')){
-			i++;
+		if((format_string[i] == '#') && (format_string[i+1] == '.') && (isdigit(format_string[i+2]))){
+			
+			// count number length in string
+			int number_length = 0;
+            for (int j = i + 2; j < strlen(format_string); j++) {
+                if (format_string[j] == 'g') {
+                    number_length = j;
+                } else if (!isdigit(format_string[j])) {
+                    break;
+                }
+            } 
+			
 		}else
 			putchar(format_string[i]);
 	}
