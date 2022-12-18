3f02821bb72aded4f9a205fcf1f659aef45ca9e2
p-olszewski <przemo1297@gmail.com>
2022-12-18 11:40:46 +0100

    LAB7 Last finishing commit

--- lab7/main.c
+++ lab7/main.c
@@ -3,8 +3,6 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-
-
 int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
 		if((format_string[i] == '#') && (format_string[i+1] == 'j')){
